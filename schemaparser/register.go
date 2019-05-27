c0c86d7d062690f54b49a05b52badd34816a800a
Ajay Gopinathan <ajaygopinathan@google.com>
2019-05-26 17:39:04 -0700

    Fix types after merge.

--- schemaparser/register.go
+++ schemaparser/register.go
@@ -73,7 +73,7 @@ func registerArtifactType(service *service.Service, ss *SchemaSet, id, namespace
 		Namespace: &pb.Namespace{
 			Name: namespace,
 		},
-		TypeProperties: make(map[string]*pb.Type),
+		Properties: make(map[string]pb.PropertyType),
 	}
 	for pname, ptype := range properties {
 		if isPropertyBuiltIn(pname) {
@@ -81,16 +81,16 @@ func registerArtifactType(service *service.Service, ss *SchemaSet, id, namespace
 		}
 		switch ptype {
 		case StringType:
-			artifactType.TypeProperties[pname] = &pb.Type{Type: &pb.Type_StringType{}}
+			artifactType.Properties[pname] = pb.PropertyType_STRING
 		case IntegerType:
-			artifactType.TypeProperties[pname] = &pb.Type{Type: &pb.Type_IntType{}}
+			artifactType.Properties[pname] = pb.PropertyType_INT
 		case NumberType:
-			artifactType.TypeProperties[pname] = &pb.Type{Type: &pb.Type_DoubleType{}}
+			artifactType.Properties[pname] = pb.PropertyType_DOUBLE
 		default:
 			return fmt.Errorf("internal error: unknown simple property type %q for property %q", ptype, pname)
 		}
 	}
-	artifactType.TypeProperties[wholeMetaPropertyName] = &pb.Type{Type: &pb.Type_StringType{}}
+	artifactType.Properties[wholeMetaPropertyName] = pb.PropertyType_STRING
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
 	defer cancel()
 	_, err = service.CreateArtifactType(ctx, &pb.CreateArtifactTypeRequest{
