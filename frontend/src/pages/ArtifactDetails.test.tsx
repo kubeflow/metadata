bd0f8375953abc1c9f9411ce681f867778d8d18c
kwasi <kwasinti@google.com>
2019-10-24 17:28:24 -0700

    Revert formatting changes

--- frontend/src/pages/ArtifactDetails.test.tsx
+++ frontend/src/pages/ArtifactDetails.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import ArtifactDetails, { ArtifactDetailsTab } from './ArtifactDetails';
-import { PageProps } from './Page';
-import { shallow, ShallowWrapper, ReactWrapper } from 'enzyme';
-import { Api } from '../lib/Api';
+import ArtifactDetails, {ArtifactDetailsTab} from './ArtifactDetails';
+import {PageProps} from './Page';
+import {shallow, ShallowWrapper, ReactWrapper} from 'enzyme';
+import {Api} from '../lib/Api';
 import * as TestUtils from '../TestUtils';
-import { RouteParams } from '../components/Router';
-import { ApiGetArtifactResponse } from '../apis/service';
+import {RouteParams} from '../components/Router';
+import {ApiGetArtifactResponse} from '../apis/service';
 
 describe('ArtifactDetails', () => {
   let tree: ShallowWrapper | ReactWrapper;
@@ -19,23 +19,23 @@ describe('ArtifactDetails', () => {
       type_id: '1',
       uri: 'gs://my-bucket/mnist',
       properties: {
-        name: { string_value: 'test model' },
-        description: { string_value: 'A really great model' },
-        version: { string_value: 'v1' },
-        create_time: { string_value: '2019-06-12T01:21:48.259263Z' },
+        name: {string_value: 'test model'},
+        description: {string_value: 'A really great model'},
+        version: {string_value: 'v1'},
+        create_time: {string_value: '2019-06-12T01:21:48.259263Z'},
         __ALL_META__: {
           string_value:
             '{"hyperparameters": {"early_stop": true, ' +
             '"layers": [10, 3, 1], "learning_rate": 0.5}, ' +
             '"model_type": "neural network", ' +
-            '"training_framework": {"name": "tensorflow", "version": "v1.0"}}',
-        },
+            '"training_framework": {"name": "tensorflow", "version": "v1.0"}}'
+        }
       },
       custom_properties: {
-        __kf_workspace__: { string_value: 'workspace-1' },
-        __kf_run__: { string_value: '1' },
-      },
-    },
+        __kf_workspace__: {string_value: 'workspace-1'},
+        __kf_run__: {string_value: '1'}
+      }
+    }
   };
   const MODEL_TYPE = 'kubeflow.org/alpha/model';
   const FAKE_MODEL_ID = '1';
@@ -47,14 +47,14 @@ describe('ArtifactDetails', () => {
       {
         params: {
           [RouteParams.ARTIFACT_TYPE]: MODEL_TYPE,
-          [RouteParams.ID]: FAKE_MODEL_ID,
-        },
+          [RouteParams.ID]: FAKE_MODEL_ID
+        }
       } as any,
       historyPushSpy,
       updateBannerSpy,
       jest.fn(),
       updateToolbarSpy,
-      jest.fn(),
+      jest.fn()
     );
   }
 
@@ -80,8 +80,8 @@ describe('ArtifactDetails', () => {
     expect(tree).toMatchSnapshot();
     expect(updateToolbarSpy).toHaveBeenLastCalledWith(
       expect.objectContaining({
-        pageTitle: 'test model (version: v1)',
-      }),
+        pageTitle: 'test model (version: v1)'
+      })
     );
   });
 
@@ -95,8 +95,8 @@ describe('ArtifactDetails', () => {
       expect.objectContaining({
         message:
           'Unable to retrieve kubeflow.org/alpha/model 1. ' + 'Click Details for more information.',
-        mode: 'error',
-      }),
+        mode: 'error'
+      })
     );
   });
 
@@ -110,8 +110,8 @@ describe('ArtifactDetails', () => {
       expect.objectContaining({
         message:
           'Unable to retrieve kubeflow.org/alpha/model 1. ' + 'Click Details for more information.',
-        mode: 'error',
-      }),
+        mode: 'error'
+      })
     );
   });
 
@@ -119,7 +119,7 @@ describe('ArtifactDetails', () => {
     mockGetArtifact.mockResolvedValue(fakeArtifactResponse);
     tree = TestUtils.mountWithRouter(<ArtifactDetails {...generateProps()} />);
     tree.setState({
-      selectedTab: ArtifactDetailsTab.OVERVIEW,
+      selectedTab: ArtifactDetailsTab.OVERVIEW
     });
 
     await mockGetArtifact;
@@ -132,7 +132,7 @@ describe('ArtifactDetails', () => {
     mockGetArtifact.mockResolvedValue(fakeArtifactResponse);
     tree = TestUtils.mountWithRouter(<ArtifactDetails {...generateProps()} />);
     tree.setState({
-      selectedTab: ArtifactDetailsTab.LINEAGE_EXPLORER,
+      selectedTab: ArtifactDetailsTab.LINEAGE_EXPLORER
     });
 
     await mockGetArtifact;
@@ -145,7 +145,7 @@ describe('ArtifactDetails', () => {
     mockGetArtifact.mockResolvedValue(fakeArtifactResponse);
     tree = TestUtils.mountWithRouter(<ArtifactDetails {...generateProps()} />);
     tree.setState({
-      selectedTab: ArtifactDetailsTab.DEPLOYMENTS,
+      selectedTab: ArtifactDetailsTab.DEPLOYMENTS
     });
 
     await mockGetArtifact;
