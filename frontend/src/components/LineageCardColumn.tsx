2afe4b660c2087851b62b20dc6e59739e138d0db
Apoorv Verma [AP] <avdaredevil@gmail.com>
2019-11-12 10:07:15 -0800

    [MLMD Lineage] Initial Lineage explorer draft implemented (#159)
    * Add metadata store protos
    
    * Add generated grpc protos for ml_metadata
    
    * Uses the version from Pipelines
    
    * Add generated grpc protos for ml_metadata
    
    * Bump `react-scripts` to 3.2.0 to support `EXTEND_ESLINT` environment
      variable, which is needed to `eslintIgnore` generated protos.
    * Add `api:metadata` script to regenerate grpc-web protos for ml_metadata.
    
    * Proxy requests to server to metadata-envoy-service
    
    * Move <rootDir>/__mocks__ into src to work around react-scripts@3.2.0 bug
      * https://github.com/facebook/create-react-app/issues/7539
    * Add `start-proxy.sh` to port forward to metadata-ui:3000
    * Regenerate ArtifactList.test.tsx.snap
      - Old snapshots genreated with node 9.9.0 fail in node 12.13 used by
        docker image
    * Whitelist @jest/* packages in gen_licenses.js and point to
      third_party/jest/LICENSE
    
    * Switch ArtifactList getArtifactTypes to ml_metadata
    
    * Major changes 1.0
    - Migrated half of lineage components
    - Rewrote all components in TS, updated and optimized to work in codebase
    - Css updated
    - **Erroneous, will fix**
    - Added gitignore
    - Added linter packages
    - Fixed spacing
    
    * Imported all elements
    
    * All of Lineage Explorer added:
    - All lineage components added
    - Card styles for target / execution cards
    - Rewrote the EdgeCavas renderer
    - Dyanmic updates and CSS updates to match live view
    - State diffing and rendering updates
    - Mocked data and presented on UI, via dynamic data
    - Z-indexing logic added to support reverse flipped connectors
    - State logic updated to match standard
    - Es6 Magic and refactor
    - Massive css updates (radio button revamp, et al)
    
    * Move changes to LineageView.tsx, which can then be used once Kwasi's changes are in
    
    * Resolved Kwasi's comments

---
+++ frontend/src/components/LineageCardColumn.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {LineageCardType, LineageRow} from './LineageTypes';
+
+import {LineageCard} from './LineageCard';
+import {EdgeCanvas} from './EdgeCanvas';
+import './LineageCardColumn.css';
+
+// Todo: Replace this with the actual interface / class used by the APIs
+export interface CardDetails {
+  title: string;
+  elements: LineageRow[];
+}
+
+export interface LineageCardColumnProps {
+  type: LineageCardType;
+  title: string;
+  cards: CardDetails[];
+  reverseBindings?: boolean;
+}
+
+export class LineageCardColumn extends React.Component<LineageCardColumnProps> {
+  public render(): JSX.Element | null {
+    const {type, title} = this.props;
+
+    return (
+      <div className={`mainColumn ${type}`}>
+        <div className='columnHeader'>
+          <h2>{title}</h2>
+        </div>
+        <div className='columnBody'>
+          {this.drawColumnContent()}
+        </div>
+      </div>
+    );
+  }
+  private jsxFromCardDetails(det: CardDetails, i: number): JSX.Element {
+    const isNotFirstEl = i > 0;
+    return <LineageCard
+      title={det.title}
+      type={this.props.type}
+      addSpacer={isNotFirstEl}
+      rows={det.elements}
+      isTarget={/Target/i.test(this.props.title)} />;
+  }
+  private drawColumnContent(): JSX.Element {
+    const {type, cards} = this.props;
+    const cardSkeleton = cards.map(c => c.elements.length);
+
+    return <React.Fragment>
+      <EdgeCanvas
+        type={type}
+        cardArray={cardSkeleton}
+        reverseEdges={!!this.props.reverseBindings} />
+      {cards.map(this.jsxFromCardDetails.bind(this))}
+    </React.Fragment>;
+  }
+}
---
+++ frontend/src/pages/LineageView.tsx
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// tslint:disable: object-literal-sort-keys
+
+import * as React from 'react';
+import {Page} from './Page';
+import {classes} from 'typestyle';
+import {commonCss} from '../Css';
+import {ListRequest} from '../lib/Api';
+import {ToolbarProps} from '../components/Toolbar';
+import {LineageCardColumn, CardDetails} from '../components/LineageCardColumn';
+
+
+interface LineageViewState {
+  columnNames: string[];
+  columnTypes: string[];
+}
+
+class LineageView extends Page<{}, LineageViewState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      columnNames: ['Input Artifact', '', 'Target', '', 'Output Artifact'],
+      columnTypes: ['ipa', 'ipx', 'target', 'opx', 'opa'],
+    };
+    this.reload = this.reload.bind(this);
+  }
+
+  public getInitialToolbarState(): ToolbarProps {
+    return {
+      actions: [],
+      breadcrumbs: [],
+      pageTitle: 'Artifacts',
+    };
+  }
+
+  public render(): JSX.Element {
+    const {columnNames} = this.state;
+    const mockInputArtifacts = [
+      {title: 'Notebook', elements: [
+        {title: 'Artifact 1', desc: 'This is really cool', next: true},
+        {title: 'Artifact 2', desc: 'This is also kinda cool', next: true},
+      ]},
+      {title: 'Datasets', elements: [
+        {title: 'Artifact w/o desc', prev: true, next: true},
+        {title: 'Artifact that should have overflowing text', desc: 'Lorem ipsum', next: true},
+      ]},
+    ] as CardDetails[];
+    const mockOutputArtifacts = [
+      {title: 'Hyperparameters', elements: [
+        {title: 'Lol', desc: 'Maybe does something', prev: true},
+        {title: 'Skip factor', desc: 'How fast should I descent the gradient', prev: true, next: true},
+      ]},
+      {title: 'Deployments', elements: [
+        {title: 'AWS Webserver', desc: 'http://foo.bar/x34s', prev: true, next: true},
+        {title: 'Product API', desc: 'Hosted via GCP', prev: true},
+      ]},
+    ] as CardDetails[];
+    const mockExec = [
+      {title: 'Execution', elements: [
+        {title: 'Some Process', desc: '13,201 Examples', prev: true, next: true}
+      ]},
+    ] as CardDetails[];
+    return (
+      <div className={classes(commonCss.page, 'LineageExplorer')} style={{flexFlow: 'row', overflow: 'auto', width: '100%', position: 'relative', background: '#f3f2f4', zIndex: 0}}>
+        <LineageCardColumn
+          type='artifact'
+          cards={mockInputArtifacts}
+          title={`${columnNames[0]}`} />
+        <LineageCardColumn
+          type='execution'
+          cards={mockExec}
+          title={`${columnNames[1]}`} />
+        <LineageCardColumn
+          type='artifact'
+          cards={[Object.assign({}, mockExec[0], {title: 'Target'})]}
+          title={`${columnNames[2]}`} />
+        <LineageCardColumn
+          type='execution'
+          cards={mockExec}
+          title={`${columnNames[3]}`} />
+        <LineageCardColumn
+          type='artifact'
+          cards={mockOutputArtifacts}
+          reverseBindings={true}
+          title={`${columnNames[4]}`} />
+      </div>
+    );
+  }
+
+  public async refresh(): Promise<void> {
+    // Todo: Implement this!
+  }
+
+  private async reload(request: ListRequest): Promise<string> {
+    // TODO: Consider making an Api method for returning and caching types
+    // if (!this.artifactTypes || !this.artifactTypes.size) {
+    //   this.artifactTypes = await this.getArtifactTypes();
+    // }
+    // const {artifacts} = this.state;
+    // if (!artifacts.length) {
+    //   try {
+    //     const response = await this.api.metadataService.listArtifacts2();
+    //     this.setState({artifacts: (response && response.artifacts) || []});
+    //     this.clearBanner();
+    //   } catch (err) {
+    //     this.showPageError('Unable to retrieve Artifacts.', err);
+    //   }
+    // }
+    // this.setState({
+    //   rows: this.getRowsFromArtifacts(request),
+    // });
+    return '';
+  }
+}
+
+export default LineageView;
