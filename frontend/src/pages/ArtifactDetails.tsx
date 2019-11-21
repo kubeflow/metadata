5a211dff2a0d1cb60ebf7d846d35251843a0b254
kwasi <kwasinti@google.com>
2019-11-21 07:28:11 -0800

    Remove Deployments tab from ArtifactDetails

--- frontend/src/pages/ArtifactDetails.tsx
+++ frontend/src/pages/ArtifactDetails.tsx
@@ -32,13 +32,11 @@ import LineageView from './LineageView';
 export enum ArtifactDetailsTab {
   OVERVIEW = 0,
   LINEAGE_EXPLORER = 1,
-  DEPLOYMENTS = 2
 }
 
 const tabs = {
   [ArtifactDetailsTab.OVERVIEW]: {name: 'Overview'},
   [ArtifactDetailsTab.LINEAGE_EXPLORER]: {name: 'Lineage Explorer'},
-  [ArtifactDetailsTab.DEPLOYMENTS]: {name: 'Deployments'}
 };
 
 const tabNames = Object.values(tabs).map(tabConfig => tabConfig.name);
