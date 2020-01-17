2494e342b37af942cef82564f237b3a1241f17c2
Apoorv Verma [AP] <avdaredevil@gmail.com>
2020-01-16 18:18:56 -0800

    [MLMD Lineage] Style changes to scale view for normal laptop screens (#199)
    * Style changes to scale view for normal laptop screens
    
    * Scrollbar changes to always show

--- frontend/src/Css.tsx
+++ frontend/src/Css.tsx
@@ -54,6 +54,16 @@ cssRaw(`
     --purple-500: #a142f4;
     --card-radius: 6px;
   }
+  ::-webkit-scrollbar {
+    -webkit-appearance: none;
+    width: 7px;
+    height: 7px;
+  }
+  ::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      background-color: rgba(0,0,0,.5);
+      box-shadow: 0 0 1px rgba(255,255,255,.5);
+  }
 `);
 
 export const color = {
