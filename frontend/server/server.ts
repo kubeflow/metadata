9de6eca48245325989e35c0f99db10810dd6b5b9
Apoorv Verma [AP] <avdaredevil@gmail.com>
2020-01-26 11:55:03 -0800

    [MLMD] Refactored server TS code (#210)
    * Server fixed, errors removed, errors take place before initialization, types added to TS file
    
    * substr -> slice, succinctness++

--- frontend/server/server.ts
+++ frontend/server/server.ts
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// tslint:disable: no-console
 
-import * as express from 'express';
-import { Application, static as StaticHandler } from 'express';
+import express, {Request, Response, NextFunction, Application, static as StaticHandler} from 'express';
 import * as proxy from 'http-proxy-middleware';
 import fetch from 'node-fetch';
 import * as path from 'path';
@@ -33,85 +33,80 @@ const {
   METADATA_ENVOY_SERVICE_SERVICE_PORT = '9090',
 } = process.env;
 
-const app = express() as Application;
-
-app.use(function (req, _, next) {
-  console.info(req.method + ' ' + req.originalUrl);
-  next();
-});
-
 if (process.argv.length < 3) {
-  console.error(`\
-Usage: node server.js <static-dir> [port].
+  console.error(`${
+    'Usage: node server.js <static-dir> [port].'}
        You can specify the API server address using the
        METADATA_SERVICE_HOST and METADATA_SERVICE_SERVICE_PORT
        env vars.`);
   process.exit(1);
 }
 
+const app = express() as Application;
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.info(req.method + ' ' + req.originalUrl);
+  next();
+});
+
 const staticDir = path.resolve(process.argv[2]);
 
 const port = process.argv[3] || 3000;
 const apiServerAddress = `http://${METADATA_SERVICE_SERVICE_HOST}:${METADATA_SERVICE_SERVICE_PORT}`;
 
-const envoyServiceAddress = `http://${METADATA_ENVOY_SERVICE_SERVICE_HOST}:${METADATA_ENVOY_SERVICE_SERVICE_PORT}`
+const envoyServiceAddress = `http://${METADATA_ENVOY_SERVICE_SERVICE_HOST}:${METADATA_ENVOY_SERVICE_SERVICE_PORT}`;
 
 const v1alpha1Prefix = 'api/v1alpha1';
 
-const clusterNameHandler = async (req, res) => {
+const clusterNameHandler = async (req: Request, res: Response) => {
   const response = await fetch(
     'http://metadata/computeMetadata/v1/instance/attributes/cluster-name',
-    { headers: {'Metadata-Flavor': 'Google' } }
+    {headers: {'Metadata-Flavor': 'Google'}}
   );
   res.send(await response.text());
 };
 
-const projectIdHandler = async (req, res) => {
+const projectIdHandler = async (req: Request, res: Response) => {
   const response = await fetch(
     'http://metadata/computeMetadata/v1/project/project-id',
-    { headers: {'Metadata-Flavor': 'Google' } }
+    {headers: {'Metadata-Flavor': 'Google'}}
   );
   res.send(await response.text());
 };
 
 app.get('/system/cluster-name', clusterNameHandler);
-app.get(BASEPATH + '/system/cluster-name', clusterNameHandler);
+app.get(`${BASEPATH}/system/cluster-name`, clusterNameHandler);
 
 app.get('/system/project-id', projectIdHandler);
-app.get(BASEPATH + '/system/project-id', projectIdHandler);
+app.get(`${BASEPATH}/system/project-id`, projectIdHandler);
 
 // Proxy metadata requests to the Envoy instance which will handle routing to the metadata gRPC server
 console.log('Starting server with gRPC proxy for /ml_metadata');
 app.all('/ml_metadata.*', proxy({
   changeOrigin: true,
-  onProxyReq: proxyReq => {
-    console.log('Metadata proxied request: ', (proxyReq as any).path);
+  onProxyReq: (proxyReq: any) => {
+    console.log('Metadata proxied request: ', proxyReq.path);
   },
   target: envoyServiceAddress,
 }));
 
-app.all('/' + v1alpha1Prefix + '/*', proxy({
+app.all(`/${v1alpha1Prefix}/*`, proxy({
   changeOrigin: true,
-  onProxyReq: proxyReq => {
-    console.log('Proxied request: ', (proxyReq as any).path);
-  },
+  onProxyReq: (proxyReq: any) => console.log('Proxied request:', proxyReq.path),
   target: apiServerAddress,
 }));
 
-app.all(BASEPATH  + '/' + v1alpha1Prefix + '/*', proxy({
+app.all(`${BASEPATH}/${v1alpha1Prefix}/*`, proxy({
   changeOrigin: true,
-  onProxyReq: proxyReq => {
-    console.log('Proxied request: ', (proxyReq as any).path);
-  },
-  pathRewrite: (path) =>
-    path.startsWith(BASEPATH) ? path.substr(BASEPATH.length, path.length) : path,
+  onProxyReq: (proxyReq: any) => console.log('Proxied request:', proxyReq.path),
+  pathRewrite: (pth: string) => pth.startsWith(BASEPATH) ? pth.slice(BASEPATH.length) : pth,
   target: apiServerAddress,
 }));
 
 app.use(BASEPATH, StaticHandler(staticDir));
 app.use(StaticHandler(staticDir));
 
-app.get('*', (req, res) => {
+app.get('*', (req: Request, res: Response) => {
   // TODO: look into caching this file to speed up multiple requests.
   res.sendFile(path.resolve(staticDir, 'index.html'));
 });
